accf40d044e4c59823d24bccbe31d63d713b4012
Ahmed Mostafa <ahmed.s.mostafa@outlook.com>
2017-04-06 16:21:55 +0200

    updated with linear regression and plotting

--- UdemyMLAZ/UdemyMLAZ/02-simple-linearregression.py
+++ UdemyMLAZ/UdemyMLAZ/02-simple-linearregression.py
@@ -1,28 +1,61 @@
 #simple linear regression model to process data in the respective folder.
 
-# 1- apply data pre-processing
-# Data Preprocessing Template
-
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
 
-# Importing the dataset
-dataset = pd.read_csv('Simple_Linear_Regression/Data.csv')
+# 1- apply data pre-processing
+# Data Preprocessing Template
+
+# 2- Importing the dataset
+
+current_dir = os.getcwd()
+csvPath = current_dir + "\\Simple_Linear_Regression\\Salary_Data.csv"
+dataset = pd.read_csv("Simple_Linear_Regression\\Salary_Data.csv")
 dataset
 
+# 3- Split X,y
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+y = dataset.iloc[:, -1].values
 
-# Splitting the dataset into the Training set and Test set
+# 4- Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
-# Feature Scaling
+# 5- Feature Scaling not needed as most libraries take care of that already
 """from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
+
+# 6- Fitting simple linear regression to the training set
+# Use Scikit-learn linear model package to fit LR to training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# 7- predicting the test results
+y_pred = regressor.predict(X_test)
+
+# 8- plot the training results in scatter chart
+plt.scatter(X_train, y_train, color='red')
+# now plot this versus the prediction of the training set itself
+plt.plot(X_train, regressor.predict(X_train), color='green')
+
+plt.title('Salary vs Experience (training set results)')
+plt.xlabel('X - years of experience')
+plt.ylabel('y - salary')
+plt.show()
+
+# 9- plot the test results in scatter chart 
+plt.scatter(X_test, y_test, color='red')
+#plot this versus the prediction on the training set itself
+plt.plot(X_train, regressor.predict(X_train), color='green')
+plt.title('Salary vs Experience (training set results)')
+plt.xlabel('X - years of experience')
+plt.ylabel('y - salary')
+plt.show()
