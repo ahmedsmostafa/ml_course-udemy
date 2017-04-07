0b6f338ff1fd115d04947c13393660debafbcc1c
Ahmed Mostafa <ahmed.s.mostafa@outlook.com>
2017-04-07 22:07:27 +0200

    updating SVR and add decision tree regression.

---
+++ UdemyMLAZ/UdemyMLAZ/06-decisiontree_regression.py
@@ -0,0 +1,40 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+np.set_printoptions(threshold=np.nan)
+
+# Importing the dataset
+dataset = pd.read_csv('..\\DecisionTree_Regression\\Position_Salaries.csv')
+dataset
+
+X = dataset.iloc[:,1:2].values
+y = dataset.iloc[:,2].values
+
+#fitting decision tree
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state = 0)
+regressor.fit(X, y)
+
+
+#prediction
+y_pred = regressor.predict(6.5)
+
+#visualize
+# Visualising the Regression results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, regressor.predict(X), color = 'blue')
+plt.title('Truth or Bluff (Regression Model)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the Regression results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (Decision Tree Regression Model)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
