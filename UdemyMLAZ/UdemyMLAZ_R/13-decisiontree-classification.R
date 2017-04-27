f933e8869f926dd37a772c55c09e94257845cdba
Ahmed Mostafa <ahmed.s.mostafa@outlook.com>
2017-04-27 14:03:53 +0200

    add and update decision tree classification stuff

--- UdemyMLAZ/UdemyMLAZ_R/13-decisiontree-classification.R
+++ UdemyMLAZ/UdemyMLAZ_R/13-decisiontree-classification.R
@@ -1,11 +1,13 @@
-# Decision Tree Classification
+dataset = read.csv("..\\DecisionTree_CLassification\\Social_Network_Ads.csv")
+dataset
 
-# Importing the dataset
-dataset = read.csv('..\\DecisionTree_CLassification\\Social_Network_Ads.csv')
-dataset = dataset[3:5]
+#select only age and salary and purahcaes
+dataset = dataset[, 3:5]
+dataset
 
 # Encoding the target feature as factor
 dataset$Purchased = factor(dataset$Purchased, levels = c(0, 1))
+dataset
 
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
@@ -19,17 +21,17 @@ test_set = subset(dataset, split == FALSE)
 training_set[-3] = scale(training_set[-3])
 test_set[-3] = scale(test_set[-3])
 
-# Fitting Decision Tree Classification to the Training set
-# install.packages('rpart')
+# Fitting classifier to the Training set
+# Create your classifier here
 library(rpart)
-classifier = rpart(formula = Purchased ~ .,
-                   data = training_set)
+classifier = rpart(formula = Purchased ~ ., data=training_set)
 
 # Predicting the Test set results
-y_pred = predict(classifier, newdata = test_set[-3], type = 'class')
+y_pred = predict(classifier, newdata = test_set[-3], type='class')
 
 # Making the Confusion Matrix
 cm = table(test_set[, 3], y_pred)
+cm
 
 # Visualising the Training set results
 library(ElemStatLearn)
@@ -38,9 +40,9 @@ X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
-y_grid = predict(classifier, newdata = grid_set, type = 'class')
+y_grid = predict(classifier, newdata = grid_set)
 plot(set[, -3],
-     main = 'Decision Tree Classification (Training set)',
+     main = 'DecisionTree Classification (Training set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
@@ -54,14 +56,14 @@ X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
-y_grid = predict(classifier, newdata = grid_set, type = 'class')
-plot(set[, -3], main = 'Decision Tree Classification (Test set)',
+y_grid = predict(classifier, newdata = grid_set)
+plot(set[, -3], main = 'DecisionTree Classification (Test set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
 points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
 points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
 
-# Plotting the tree
+#plot the decision tree
 plot(classifier)
 text(classifier)
