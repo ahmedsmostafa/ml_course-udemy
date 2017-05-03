02c4fb69899df207a576697847efbd73021bb82f
Ahmed Mostafa <ahmed.s.mostafa@outlook.com>
2017-05-03 19:14:20 +0200

    update apriori python and r code

--- UdemyMLAZ/UdemyMLAZ/17-associationruleslearning_apriori.py
+++ UdemyMLAZ/UdemyMLAZ/17-associationruleslearning_apriori.py
@@ -7,11 +7,33 @@ cwd = os.getcwd()
 np.set_printoptions(threshold=np.nan)
 
 # Importing the dataset
+import sys
+
 try:
-    dataset = pd.read_csv('..\\AssociationRulesLearning_Apriori\\Market_Basket_Optimisation.csv', heder=None)
+    dataset = pd.read_csv('..\\AssociationRulesLearning_Apriori\\Market_Basket_Optimisation.csv', header=None)
+    #there is no apriori implementation in python?
+    #using the file given in our examples apyori.py
+    #sys.path.insert(0, '..\\AssociationRulesLearning_Apriori\\')
 except:
+    #there is no apriori implementation in python?
+    #using the file given in our examples apyori.py
     dataset = pd.read_csv('UdemyMLAZ\\AssociationRulesLearning_Apriori\\Market_Basket_Optimisation.csv', header=None)
+    #sys.path.insert(0, 'UdemyMLAZ\\AssociationRulesLearning_Apriori\\')
 
 dataset
 
-#extract features & outcomes from data
+# convert dataset to a sparse matrix
+#create empty vector/list
+#use dataset.values to navigate through them by index
+#surround the result by string to make sure nan is set to string 
+#surround the whole line by a [] to make sure each row is a list itself
+transactions = []
+for i in range(0 , len(dataset)):
+    transactions.append([str(dataset.values[i,j]) for j in range(0,len(dataset.columns))])
+
+# import the local apriori implementation
+from apyori import apriori
+rules = apriori(transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
+
+result = list(rules)
+result
--- UdemyMLAZ/UdemyMLAZ_R/17-associationruleslearning_apriori.R
+++ UdemyMLAZ/UdemyMLAZ_R/17-associationruleslearning_apriori.R
@@ -4,13 +4,15 @@ library(arules)
 dataset = read.csv("..\\AssociationRulesLearning_Apriori\\Market_Basket_Optimisation.csv", header = FALSE)
 
 #use read transactions to build a sparse matrix
-dataset = read.transactions("..\\AssociationRulesLearning_Apriori\\Market_Basket_Optimisation.csv", sep = ',', rm.duplicates = TRUE)
+dataset = read.transactions("..\\AssociationRulesLearning_Apriori\\Market_Basket_Optimisation.csv", sep = ',', rm.duplicates = FALSE)
 
 summary(dataset)
 
 #plot items frequency
-itemFrequencyPlot(dataset, topN=100)
+itemFrequencyPlot(dataset, topN=10)
 
 #training apriori
-rules = apriori(data = dataset, parameter = list(support = 0.02, confidence = 0.4))
+rules = apriori(data = dataset, parameter = list(support = 0.004, confidence = 0.2))
 
+#insepction of rules
+inspect(sort(rules, by = 'lift')[1:10])
