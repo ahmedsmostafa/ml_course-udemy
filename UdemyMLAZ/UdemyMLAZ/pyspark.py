7b37ed989cb2f99374765565f4366b2d76d494d2
Ahmed Mostafa <ahmed.s.mostafa@outlook.com>
2017-05-15 07:04:41 +0200

    adding pycharm data

--- UdemyMLAZ/UdemyMLAZ/pyspark.py
+++ UdemyMLAZ/UdemyMLAZ/pyspark.py
@@ -1,2 +1,21 @@
-from operator import add
 from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+
+sc = SparkContext(appName="simpleApp", master="local[2]")
+print("spark context version is: " + sc.version)
+
+ssc = StreamingContext(sc,1)
+print("spark streaming context created")
+
+from pyspark.sql import SparkSession
+spark = SparkSession(sc)
+print("spark session version is: " + spark.version)
+
+lines = ssc.socketTextStream("localhost", 9999)
+
+words = lines.flatMap(lambda line: line.split(" "))
+
+pairs = words.map(lambda word: (word, 1))
+wordCounts = pairs.reduceByKey(lambda x,y: x + y)
+
+wordCounts.pprint()
