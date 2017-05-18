094471487f7f34b97821ebf80089ba9b79169d97
Ahmed Mostafa <ahmed.s.mostafa@outlook.com>
2017-05-19 00:16:43 +0200

    adding kernel pca in python

--- UdemyMLAZ/DimensionalityReduction_PCA_Kernel/kernel_pca.py
+++ UdemyMLAZ/DimensionalityReduction_PCA_Kernel/kernel_pca.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Social_Network_Ads.csv')
+dataset = pd.read_csv('..\\DimensionalityReduction_PCA_Kernel\\Social_Network_Ads.csv')
 X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
 
--- UdemyMLAZ/UdemyMLAZ/26-dimensionreduction-pca_kernel.py
+++ UdemyMLAZ/UdemyMLAZ/26-dimensionreduction-pca_kernel.py
@@ -19,7 +19,7 @@ X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 # Feature Scaling
@@ -28,7 +28,16 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Fitting classifier to the Training set
+# APPLY KERNEL PCA
+from sklearn.decomposition import KernelPCA
+kpca = KernelPCA(n_components = 2)
+X_train = kpca.fit_transform(X_train)
+X_test = kpca.transform(X_test)
+
+#fitting logistic regression
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train,y_train)
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
@@ -56,8 +65,8 @@ for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
 plt.title('SVM Kernel (Training set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
 plt.legend()
 plt.show()
 
@@ -74,7 +83,7 @@ for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
 plt.title('SVM Kernel (Test set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
 plt.legend()
 plt.show()
