1bf44a0ceee56280fa5a7d2222e0a1b7ef08acf1
Ahmed Mostafa <ahmed.s.mostafa@outlook.com>
2017-05-21 23:21:52 +0200

    finish model selection code for grid search and kfold

--- UdemyMLAZ/ModelSelection/grid_search.R
+++ UdemyMLAZ/ModelSelection/grid_search.R
@@ -1,7 +1,7 @@
 # Grid Search
 
 # Importing the dataset
-dataset = read.csv('Social_Network_Ads.csv')
+dataset = read.csv("..\\ModelSelection\\Social_Network_Ads.csv")
 dataset = dataset[3:5]
 
 # Encoding the target feature as factor
--- UdemyMLAZ/ModelSelection/k_fold_cross_validation.R
+++ UdemyMLAZ/ModelSelection/k_fold_cross_validation.R
@@ -1,7 +1,7 @@
 # k-Fold Cross Validation
 
 # Importing the dataset
-dataset = read.csv('Social_Network_Ads.csv')
+dataset = read.csv('..\\ModelSelection\\Social_Network_Ads.csv')
 dataset = dataset[3:5]
 
 # Encoding the target feature as factor
